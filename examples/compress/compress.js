1572c00c9d860bcc13fd62c75902a75ae9f52f20
Young Jay <hiyoungjay@gmail.com>
2012-05-14 23:48:10 +0800

    beautify compress.js

--- examples/compress/compress.js
+++ examples/compress/compress.js
@@ -6,9 +6,9 @@ node compress.js /home/path/to/src.html
 output:
 /home/path/to/index-min.js
 
-process: input a html file
+process:
 1  find the "<script>" tags
-2  read the script src when availible.
+2  read the script src
 3  combine them and use "uglify-js" to compress them into one file
 4  write it into srcPath/outputFileName
 
@@ -16,9 +16,8 @@ process: input a html file
 
 var fs = require('fs');
 var path = require('path');
-var next = require('../../index.js');
-
 var uglify = require('uglify-js');
+var next = require('../../index.js');
 
 var fileName = process.argv[2];
 var filePath = path.dirname(fileName);
@@ -29,7 +28,7 @@ var rscript = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
 var run = next.pipe(
   function(fileName, callback) {
     if (!fileName) {
-      callback('调用的时候请输入文件名，例如:node compress.js js.html');
+      callback('调用的时候请输入文件名，例如:node compress.js code.html');
     } else {
       callback(null, fileName);
     }
@@ -64,7 +63,7 @@ var run = next.pipe(
     console.log('compressing content...')
     callback(null, uglify(contents.join('')));
   },
-  
+
   function(distCode, callback) {
     var distFileName = path.resolve(path.join(filePath, outputFileName));
     console.log('write file to: ' + distFileName);
